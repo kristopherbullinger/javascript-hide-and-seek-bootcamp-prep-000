9ab9314d1a24b54f328e5877505bc6f177e17e84
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-04-23 00:49:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 function getFirstSelector(selector) {
-  var e = document.querySelector(selector)
-  return e
+  var e = document.querySelector(selector);
+  return e;
 }
 
 function nestedTarget() {
-  var e = document.querySelector('#nested .target')
-  return e
+  var e = document.querySelector('#nested .target');
+  return e;
 }
 
 function increaseRankBy(n) {
   var e = document.querySelectorAll('.ranked-list');
   for (let i=0;i<e.length;i++) {
-    e[i].innerHTML = parseInt(e[i].innerHTML) + n
+    e[i].innerHTML = parseInt(e[i].innerHTML) + n;
   }
 }
 
 function deepestChild() {
-  var f = document.querySelector('#grand-node')
-  var g = f.childNodes
-  var c
+  var f = document.querySelector('#grand-node');
+  var g = f.childNodes;
+  var c;
    if (f.firstChild) {
     while (g.length > 1) {
       for (let i=0;i<g.length;i++) {
         if (g[i].firstChild) {
           g.push(g[i].childNodes);
-          c = g.shift()
+          g.shift();
         } else {g.shift()}
       }
-      return g
+      return g;
     }
      
    } else {return f}
