ef991b6837925894d9aa572fcc0204de6c3a4139
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-04-23 00:16:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector) {
+  var e = document.querySelector(selector)
+  return e
+}
+
+function nestedTarget() {
+  var e = document.querySelector('#nested .target')
+  return e
+}
+
+function increaseRankBy(n) {
+  var e = document.querySelectorAll('.ranked-list');
+  for (let i=0;i<e.length;i++) {
+    e[i].innerHTML = parseInt(e[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var f = document.querySelector('#grand-node')
+  var g = f.childNodes
+  return g
+}
+
+/*function deepestChild() {
+  var f = document.querySelector('#grand-node')
+  var nextF = f.firstChild 
+  while(nextF) {
+    f = nextF 
+    nextF = f.firstChild
+  }
+  return f
+}*/
